27a6f2fb5054819ede602f370b30f89d4e97db62
joereger <joereger@d42e49d7-e3ba-4d4b-9865-d14956a61029>
2011-02-03 21:42:27 +0000

    Tomcats weren't shutting down.  Had to add a pkill command to force them dead.  Take that mofo bytes.

--- egg/egg-tomcat-stop.sh
+++ egg/egg-tomcat-stop.sh
@@ -13,3 +13,10 @@ ssh -t -t $HOST "sudo chmod -R 755 /home/ec2-user/egg/$APPDIR"
 ssh -t -t $HOST "cd egg/$APPDIR/tomcat/bin/; chmod 777 *.sh;"
 #ssh -t -t $HOST "export CATALINA_HOME=/home/ec2-user/egg/$APPDIR/tomcat; bash egg/$APPDIR/tomcat/bin/catalina.sh stop"
 ssh $HOST "export CATALINA_HOME=/home/ec2-user/egg/$APPDIR/tomcat; export JRE_HOME=/usr/lib/jvm/jre; bash egg/$APPDIR/tomcat/bin/shutdown.sh"
+echo "Waiting 5 seconds for tomcat to shut down before sending hard pkill"
+sleep 5
+ssh -t -t $HOST "pkill -f egg/mythredz1/tomcat/conf"
+
+#ssh $HOST "ps -ef | grep $APPDIR | grep java | cut -d ' ' -f2"
+
+
