d9fd009636d14ab371c8ba0c9296023d8b9d0c89
joereger <joereger@d42e49d7-e3ba-4d4b-9865-d14956a61029>
2011-03-13 17:07:27 +0000

    improved pulse performance

--- egg/pulse.sh
+++ egg/pulse.sh
@@ -61,21 +61,6 @@ do
     exec 3<> $INSTANCESFILE; while read ininstancesline <&3; do {
         if [ $(echo "$ininstancesline" | cut -c1) != "#" ]; then
             LOGICALINSTANCEID=$(echo "$ininstancesline" | cut -d ":" -f1)
-            SECURITYGROUP=$(echo "$ininstancesline" | cut -d ":" -f2)
-            INSTANCESIZE=$(echo "$ininstancesline" | cut -d ":" -f3)
-            AMIID=$(echo "$ininstancesline" | cut -d ":" -f4)
-            ELASTICIP=$(echo "$ininstancesline" | cut -d ":" -f5)
-            EBSVOLUME=$(echo "$ininstancesline" | cut -d ":" -f6)
-            EBSDEVICENAME=$(echo "$ininstancesline" | cut -d ":" -f7)
-            exec 4<> $AMAZONIIDSFILE; while read amazoniidsline <&4; do {
-                if [ $(echo "$amazoniidsline" | cut -c1) != "#" ]; then
-                    LOGICALINSTANCEID_A=$(echo "$amazoniidsline" | cut -d ":" -f1)
-                    if [ "$LOGICALINSTANCEID_A" == "$LOGICALINSTANCEID" ]; then
-                        AMAZONINSTANCEID=$(echo "$amazoniidsline" | cut -d ":" -f2)
-                        HOST=$(echo "$amazoniidsline" | cut -d ":" -f3)
-                    fi
-                fi
-            }; done; exec 4>&-
             ROWS=$(( $ROWS + 1 ))
             tput cup $ROWS 1
             tput el
